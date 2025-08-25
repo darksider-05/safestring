559fafb8cafb7af6f1b643c729374bcdd266c1bc
darksider-05 <a.bahrkazemi84@gmail.com>
2025-08-26 01:08:21 +0330

    a major security update. now, harder to crack.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+*a simple cmd-line app that obscures strings within the defined range into a hard to crack sequence.*
+
+
+# notice:
+***do not use this app for security of sensitive data. though it repels most noobs, it would take only minutes for a professional to crack. do not confuse obscurity with encryption***
+
+to defend your data from fellow users of this script, i recommend reassigning the values in the baseMap variable.
+\
+you might even play around with the transformations a bit also.
+
+
--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 void main() {
   var cipher = Cipher();
@@ -6,7 +7,8 @@ void main() {
   bool loop1 = true;
   String? mode;
 
-  stdout.writeln("Choose one:");
+  stdout.writeln("Choose an option:");
+  stdout.writeln();
   stdout.writeln("1. Encode");
   stdout.writeln("2. Decode");
 
@@ -28,17 +30,34 @@ void main() {
     String exp = stdin.readLineSync() ?? "";
     List<String> lst = exp.split("");
     List<int> vals = [];
+    List tricks = [];
 
     for (int index = 0; index < lst.length; index++) {
       vals.add(cipher.val(lst[index]));
     }
     vals = encoder.encode(vals);
+
+    for (int index = 0; index < vals.length; index++) {
+      int a = Random().nextInt(10);
+      if (a > 5) {
+        tricks.add(index);
+      }
+    }
+
+    for (var item in tricks) {
+      vals.insert(item, [89, 78, 93, 69, 91, 94, 70, 87][Random().nextInt(8)]);
+    }
+
     stdout.writeln("Your encoded expression:");
     stdout.writeln(vals.join("A"));
   } else if (mode == "decode") {
     stdout.writeln("Please enter the sequence:");
     String exp = stdin.readLineSync() ?? "";
     List<String> lst = exp.split("A");
+
+    lst.removeWhere(
+      (item) => ["89", "78", "93", "69", "91", "94", "70", "87"].contains(item),
+    );
 
     List<int> vals = lst.map((numStr) => int.tryParse(numStr) ?? 999).toList();
     vals = encoder.decode(vals);
@@ -48,8 +67,10 @@ void main() {
     }
 
     stdout.writeln("Your decoded expression:");
+    stdout.writeln();
     stdout.writeln(lst.join(""));
   }
+  stdin.readLineSync();
 }
 
 class Encoder {
@@ -68,30 +89,66 @@ class Encoder {
   }
 
   int enc(int val, int index) {
-    switch (index % 4) {
+    switch (index % 13) {
       case 0:
-        return val + 1;
+        return val + 7;
       case 1:
-        return val + 2;
+        return val + 9;
       case 2:
-        return val + 3;
+        return val + 8;
       case 3:
         return val + 4;
+      case 4:
+        return val + 5;
+      case 5:
+        return val + 11;
+      case 6:
+        return val + 2;
+      case 7:
+        return val + 2;
+      case 8:
+        return val + 1;
+      case 9:
+        return val + 6;
+      case 10:
+        return val + 3;
+      case 11:
+        return val + 10;
+      case 12:
+        return val + 12;
       default:
         return 999;
     }
   }
 
   int dec(int val, int index) {
-    switch (index % 4) {
+    switch (index % 13) {
       case 0:
-        return val - 1;
+        return val - 7;
       case 1:
-        return val - 2;
+        return val - 9;
       case 2:
-        return val - 3;
+        return val - 8;
       case 3:
         return val - 4;
+      case 4:
+        return val - 5;
+      case 5:
+        return val - 11;
+      case 6:
+        return val - 2;
+      case 7:
+        return val - 2;
+      case 8:
+        return val - 1;
+      case 9:
+        return val - 6;
+      case 10:
+        return val - 3;
+      case 11:
+        return val - 10;
+      case 12:
+        return val - 12;
       default:
         return 999;
     }
@@ -99,132 +156,76 @@ class Encoder {
 }
 
 class Cipher {
-  final baseMap = {
-    'a': 0,
-    'A': 31,
-    'b': 1,
-    'B': 32,
-    'c': 2,
-    'C': 33,
-    'd': 3,
-    'D': 34,
-    'e': 4,
-    'E': 35,
-    'f': 5,
-    'F': 36,
-    'g': 6,
-    'G': 37,
-    'h': 7,
-    'H': 38,
-    'i': 8,
-    'I': 39,
-    'j': 9,
-    'J': 40,
-    'k': 10,
-    'K': 41,
-    'l': 11,
-    'L': 42,
-    'm': 12,
-    'M': 43,
-    'n': 13,
-    'N': 44,
-    'o': 14,
-    'O': 45,
-    'p': 15,
-    'P': 46,
-    'q': 16,
-    'Q': 47,
-    'r': 17,
-    'R': 48,
-    's': 18,
-    'S': 49,
-    't': 19,
-    'T': 50,
-    'u': 20,
-    'U': 51,
-    'v': 21,
-    'V': 52,
-    'w': 22,
-    'W': 53,
-    'x': 23,
-    'X': 54,
-    'y': 24,
-    'Y': 55,
-    'z': 25,
-    'Z': 56,
-    ' ': 26,
-    '"': 27,
-    '?': 28,
-    '.': 29,
-    ',': 30,
-  };
-  final reverseMap = {
-    0: 'a',
-    1: 'b',
-    2: 'c',
-    3: 'd',
-    4: 'e',
-    5: 'f',
-    6: 'g',
-    7: 'h',
-    8: 'i',
-    9: 'j',
-    10: 'k',
-    11: 'l',
-    12: 'm',
-    13: 'n',
-    14: 'o',
-    15: 'p',
-    16: 'q',
-    17: 'r',
-    18: 's',
-    19: 't',
-    20: 'u',
-    21: 'v',
-    22: 'w',
-    23: 'x',
-    24: 'y',
-    25: 'z',
-    26: ' ',
-    27: '"',
-    28: '?',
-    29: '.',
-    30: ',',
-    31: 'A',
-    32: 'B',
-    33: 'C',
-    34: 'D',
-    35: 'E',
-    36: 'F',
-    37: 'G',
-    38: 'H',
-    39: 'I',
-    40: 'J',
-    41: 'K',
-    42: 'L',
-    43: 'M',
-    44: 'N',
-    45: 'O',
-    46: 'P',
-    47: 'Q',
-    48: 'R',
-    49: 'S',
-    50: 'T',
-    51: 'U',
-    52: 'V',
-    53: 'W',
-    54: 'X',
-    55: 'Y',
-    56: 'Z',
-  };
+  final Map<String, int> baseMap;
+  final Map<int, String> reverseMap;
 
-  int val(String char) {
-    final base = baseMap[char] ?? 999;
-    return base;
+  Cipher()
+    : this.baseMap = {
+        'a': 0,
+        'A': 31,
+        'b': 1,
+        'B': 32,
+        'c': 2,
+        'C': 33,
+        'd': 3,
+        'D': 34,
+        'e': 4,
+        'E': 35,
+        'f': 5,
+        'F': 36,
+        'g': 6,
+        'G': 37,
+        'h': 7,
+        'H': 38,
+        'i': 8,
+        'I': 39,
+        'j': 9,
+        'J': 40,
+        'k': 10,
+        'K': 41,
+        'l': 11,
+        'L': 42,
+        'm': 12,
+        'M': 43,
+        'n': 13,
+        'N': 44,
+        'o': 14,
+        'O': 45,
+        'p': 15,
+        'P': 46,
+        'q': 16,
+        'Q': 47,
+        'r': 17,
+        'R': 48,
+        's': 18,
+        'S': 49,
+        't': 19,
+        'T': 50,
+        'u': 20,
+        'U': 51,
+        'v': 21,
+        'V': 52,
+        'w': 22,
+        'W': 53,
+        'x': 23,
+        'X': 54,
+        'y': 24,
+        'Y': 55,
+        'z': 25,
+        'Z': 56,
+        ' ': 26,
+        '"': 27,
+        '?': 28,
+        '.': 29,
+        ',': 30,
+      },
+      this.reverseMap = {} {
+    for (var entry in baseMap.entries) {
+      reverseMap[entry.value] = entry.key;
+    }
   }
 
-  String char(int val) {
-    final charac = reverseMap[val] ?? "^";
-    return charac;
-  }
+  int val(String char) => baseMap[char] ?? 999;
+
+  String char(int val) => reverseMap[val] ?? "^";
 }
